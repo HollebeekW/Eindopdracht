85bd7a72b0a85c315397fcecc0f7279dd8de2fcb
Wesley Hollebeek <hollebeekw@gmail.com>
2023-04-12 13:09:45 +0200

    Added comments to RegistrationViewModel

--- ViewModels/RegistrationViewModel.cs
+++ ViewModels/RegistrationViewModel.cs
@@ -20,6 +20,7 @@ namespace Eindopdracht.ViewModels
         public UserModel User { get; set; }
         public RelayCommand AddUserCommand { get; set; }
 
+        //Bind Textboxes from view
         private string _email;
         private string _firstName;
         private string _lastName;
@@ -75,20 +76,25 @@ namespace Eindopdracht.ViewModels
         public RegistrationViewModel()
         {
             User = new UserModel();
+
+            //Create instance of relaycommand, so button from view executes function "AddUser"
             AddUserCommand = new RelayCommand(AddUser);
         }
         private void AddUser()
         {
+            //Check if all textboxes are filled in
             if (string.IsNullOrWhiteSpace(Email) || string.IsNullOrWhiteSpace(FirstName) || string.IsNullOrEmpty(LastName) || string.IsNullOrWhiteSpace(Password) || string.IsNullOrEmpty(ConfirmPassword))
             {
                 MessageBox.Show("Vul alle velden in");
             }
             
+            //Check if Password and Password Confirmation match
             if (Password != ConfirmPassword)
             {
                 MessageBox.Show("Wachtwoorden zijn niet hetzelfde");
             }
 
+            //If all checks pass, execute query code
             else
             {
                 var User = new UserModel
@@ -101,23 +107,31 @@ namespace Eindopdracht.ViewModels
 
                 try
                 {
+                    //Database connection
                     using (var connection = new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=MYDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False"))
                     {
+                        //Query to insert users' data into Users table
                         var query = "INSERT INTO Users (Email,FirstName,Lastname,Password) VALUES (@Email,@FirstName,@LastName,@Password)";
 
                         using (var command = new SqlCommand(query, connection))
                         {
+                            //Change all data types to NVarChar, to match database table
                             command.Parameters.Add("@Email", System.Data.SqlDbType.NVarChar).Value = User.Email;
                             command.Parameters.Add("@FirstName", System.Data.SqlDbType.NVarChar).Value = User.FirstName;
                             command.Parameters.Add("@LastName", System.Data.SqlDbType.NVarChar).Value = User.LastName;
                             command.Parameters.Add("@Password", System.Data.SqlDbType.NVarChar).Value = User.Password;
 
                             connection.Open();
+
+                            //store amount of added rows in integer "RowsAdded" while executing query
                             int RowsAdded = command.ExecuteNonQuery();
+
+                            //if RowsAdded > 0 (meaning row was succesfully added) show message
                             if (RowsAdded > 0)
                             {
                                 MessageBox.Show("Account geregistreerd");
                             }
+                            //Show error if row was not added
                             else
                             {
                                 MessageBox.Show("Error");
