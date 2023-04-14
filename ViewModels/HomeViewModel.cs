d03e5f9e31712f07172c529ac79280fd83d04daf
Wesley Hollebeek <hollebeekw@gmail.com>
2023-04-14 11:45:16 +0200

    Finished ability to add authors, including checks to prevent empty and duplicate authors

--- ViewModels/HomeViewModel.cs
+++ ViewModels/HomeViewModel.cs
@@ -2,6 +2,7 @@
 using Eindopdracht.Models;
 using Eindopdracht.Views;
 using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Identity.Client;
 using System;
 using System.Collections.Generic;
@@ -19,6 +20,84 @@ namespace Eindopdracht.ViewModels
 {
     public class HomeViewModel : INotifyPropertyChanged
     {
+        public RelayCommand AddAuthorCommand { get; set; }
+        public AuthorModel Author { get; set; }
+
+        private string _firstName;
+        private string _lastName;
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set
+            {
+                _firstName = value;
+                OnPropertyChanged(nameof(_firstName));
+            }
+        }
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set
+            {
+                _lastName = value;
+                OnPropertyChanged(nameof(_lastName));
+            }
+        }
+
+        public HomeViewModel()
+        {
+            Author = new AuthorModel();
+            AddAuthorCommand = new RelayCommand(AddAuthor);
+        }
+
+        private void AddAuthor()
+        {
+            if (string.IsNullOrWhiteSpace(FirstName) || string.IsNullOrWhiteSpace(LastName))
+            {
+                MessageBox.Show("Vul alle velden in");
+            }
+
+            else
+            {
+                try
+                {
+                    var newAuthor = new AuthorModel()
+                    {
+                        FirstName = FirstName,
+                        LastName = LastName
+                    };
+
+                    var optionsBuilder = new DbContextOptionsBuilder<MyDbContext>();
+
+                    using (var contextCheckAuthors = new MyDbContext(optionsBuilder.Options))
+                    {
+                        var count = contextCheckAuthors.Authors.Count(a => a.FirstName == FirstName && a.LastName == LastName);
+
+                        if (count == 0)
+                        {
+                            using (var contextAddAuthor = new MyDbContext(optionsBuilder.Options))
+                            {
+                                contextAddAuthor.Authors.Add(newAuthor);
+                                contextAddAuthor.SaveChanges();
+                            }
+
+                            MessageBox.Show("Auteur toegevoegd");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Deze auteur is al toegevoegd");
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show(e.ToString());
+                }
+            }
+        }
+
         public event PropertyChangedEventHandler PropertyChanged;
 
         protected virtual void OnPropertyChanged(string propertyName)
